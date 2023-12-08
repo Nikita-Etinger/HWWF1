07dcc93a2a1ece67e49429d20851d879ae472a7d
Nikita Etinger <113473133+Nikita-Etinger@users.noreply.github.com>
2023-12-08 20:52:52 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Задание 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(25, 54);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Задание 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Location = new Point(12, 199);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(318, 196);
+            panel1.TabIndex = 3;
+            panel1.MouseClick += panel1_MouseClick;
+            panel1.MouseEnter += panel1_MouseEnter;
+            panel1.MouseLeave += panel1_MouseLeave;
+            panel1.MouseMove += panel1_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 398);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 50);
+            label1.TabIndex = 4;
+            label1.Text = "OUT";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 166);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.No;
+            label2.Size = new Size(40, 30);
+            label2.TabIndex = 5;
+            label2.Text = "0:0";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(173, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 30);
+            label3.TabIndex = 6;
+            label3.Text = "Ctrl is: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(255, 166);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 30);
+            label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(273, 414);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 30);
+            label5.TabIndex = 8;
+            label5.Text = "work";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(342, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            MouseMove += Form1_MouseMove;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+using System.Drawing.Drawing2D;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Icon customIcon = new Icon("icon.ico");
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Профессиональный разработчик с опытом работы более X лет в области программирования на C#, C++ и использования Microsoft SQL Server. Обладаю глубокими знаниями и практическим опытом в разработке масштабируемых и эффективных приложений, отвечающих высоким стандартам качества.");
+            list.Add("Программирование на C# и C++: опыт разработки приложений с использованием современных практик и стандартов языка, включая многозадачность, обработку ошибок и оптимизацию производительности.");
+            list.Add("Базы данных: глубокие знания в проектировании, оптимизации и обслуживании баз данных с использованием Microsoft SQL Server. Опыт работы с T-SQL для создания эффективных запросов и процедур.");
+            list.Add("Архитектура приложений: опыт проектирования и разработки модульных, масштабируемых и поддерживаемых приложений с применением принципов объектно-ориентированного программирования.");
+
+
+
+            int wordCount = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                wordCount += list[i].Length;
+                MessageBox.Show(list[i], "РЕЗЮМЕ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            wordCount += list[list.Count - 1].Length;
+            MessageBox.Show(list[list.Count - 1], "" + (wordCount / list.Count), MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 2001);
+            int attempts = 0;
+            int lastNum = 0;
+
+            MessageBox.Show("Загадано число от 1 до 2000", "Игра", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            while (true)
+            {
+                string userInput = Microsoft.VisualBasic.Interaction.InputBox("Введите ваше предположение:" + (lastNum != 0 ? "\n\nПрошлое введенное число: " + lastNum : ""), "Игра", "", -1, -1);
+
+                // был ли введен пустой ответ
+                if (userInput == "")
+                {
+                    DialogResult result = MessageBox.Show("Вы уверены, что хотите завершить игру?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                        break;
+                    else
+                        continue;
+                }
+
+                if (!int.TryParse(userInput, out int userGuess))
+                {
+                    MessageBox.Show("Введите корректное число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    continue;
+                }
+                lastNum = userGuess;
+
+                attempts++;
+
+                if (userGuess == targetNumber)
+                {
+                    MessageBox.Show($"Вы угадали число {targetNumber} за {attempts} попыток.", "Игра", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                }
+                else if (userGuess < targetNumber)
+                {
+                    MessageBox.Show("Загаданное число больше вашего предположения.", "Игра", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Загаданное число меньше вашего предположения.", "Игра", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool checkCtrl()
+        {
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                label4.Text = "push";
+                return true;
+            }
+            else label4.Text = "unpush";
+            return false;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int mouseX = e.X;
+            int mouseY = e.Y;
+
+
+            Point clientCoordinates = panel1.PointToClient(new Point(mouseX, mouseY));
+            //Point screenCoordinates = panel1.PointToScreen(new Point(mouseX, mouseY));
+
+            //Form1.ActiveForm.Text = clientCoordinates.X + ":" + clientCoordinates.Y;
+            //реализация не возможна,не возможно использовать заголовок одновременно для размера окна и для отображения координаты мыши(перебивают друг друга)
+            label2.Text = $"{clientCoordinates.X} : {clientCoordinates.Y}";
+            checkCtrl();
+
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            checkCtrl();
+            label1.Text = "IN";
+
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "OUT";
+            checkCtrl();
+        }
+
+
+
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            checkCtrl();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Size clientSize = this.ClientSize;
+                Form1.ActiveForm.Text = $"Ширина: {clientSize.Width}| Высота: {clientSize.Height}";
+            }
+            if (checkCtrl())
+            {
+
+                label5.Text = "exit";
+                //Close();
+            }
+        }
+    }
+}
